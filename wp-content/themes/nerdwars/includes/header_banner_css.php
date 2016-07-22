9652c06ecbd7bd18d4972ba31a94726ac8453b42
vinicius <vinicius>
2016-07-21 21:10:59 -0300

    fixed deploy

--- wp-content/themes/nerdwars/includes/header_banner_css.php
+++ wp-content/themes/nerdwars/includes/header_banner_css.php
@@ -2,18 +2,18 @@
     <?php 
         
         $badge = rand(0, 6);
-        $bg = rand(0, 1);
+        $bg    = rand(0, 1);
     ?>
     
     /*HEADER BGs ALEATORIOS*/
     #header #titleBg{
         min-height: 340px;
-        background-image: url('/nerdwars/wp-content/themes/nerdwars/imagens/bg_<?=$bg?>_header-0.jpg');
+        background-image: url('/wp-content/themes/nerdwars/imagens/bg_<?=$bg?>_header-0.jpg');
         background-position: center bottom;
     }
 
     #header #titleBg #titleBg2{
-        background: url('/nerdwars/wp-content/themes/nerdwars/imagens/bg_<?=$bg?>_header-1.png') no-repeat;
+        background: url('/wp-content/themes/nerdwars/imagens/bg_<?=$bg?>_header-1.png') no-repeat;
         background-size: contain;
         background-position: center bottom;
         min-height: 340px;
@@ -32,6 +32,6 @@
         height: 340px;
         display: block;
         border-radius: 220px;
-        background: url('/nerdwars/wp-content/themes/nerdwars/imagens/badge-<?=$badge?>.png') no-repeat;
+        background: url('/wp-content/themes/nerdwars/imagens/badge-<?=$badge?>.png') no-repeat;
     }
 </style>
